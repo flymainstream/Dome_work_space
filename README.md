7d92e5deebc8209b10b6b15b3e6fbc6094b3d59f
liujintao <myseeyouletplease@outlook.com>
2021-03-10 10:06:40 +0800

    RedisDome commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Dome space 
 made of Jim , The  dome project of ...... 
 
+'RedisDome.java'  Dome of redis . using Spring-Data-Redis  
+
--- src/main/java/com/js/spring/data/redis/RedisDome.java
+++ src/main/java/com/js/spring/data/redis/RedisDome.java
@@ -3,12 +3,12 @@ package com.js.spring.data.redis;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.BoundValueOperations;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
 
-import javax.annotation.Resource;
+import java.util.ArrayList;
 
 /**
  * @author 刘锦涛
@@ -19,24 +19,44 @@ import javax.annotation.Resource;
  * @description: TODO
  */
 public class RedisDome {
+    /**
+     * begin
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        RedisDome redisDome = new RedisDome();
+
+//        CreateData createData = new CreateData(redisDome);
+        redisDome.query(redisDome);
+    }
+
+
+    private void query(RedisDome redisDome) {
+        queryStr(redisDome);
+        queryList(redisDome);
+
+    }
+
+    private void queryList(RedisDome redisDome) {
+
+        String twice = redisDome.getRedisTemplate().opsForList().leftPop("Twice");
+        System.out.println(
+                twice
+        );
+    }
+
+    private void queryStr(RedisDome redisDome) {
+        String once = redisDome.getRedisTemplate().opsForValue().get("once");
+        System.out.println(
+                once
+        );
+    }
+
 
     private RedisTemplate<String, String> redisTemplate;
 
-
-    public RedisDome() {
-        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
-        redisConfiguration.setPort(6379);
-        redisConfiguration.setHostName("192.168.79.128");
-//        redisConfiguration.setUsername();
-        redisConfiguration.setPassword("redis");
-//        redisConfiguration.setDatabase();
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisConfiguration);
-        jedisConnectionFactory.setUsePool(true);
-
-
-        this.redisTemplate = getRedisTemplate(jedisConnectionFactory);
-        this.redisTemplate.afterPropertiesSet();
-    }
 
     private RedisTemplate<String, String> getRedisTemplate(JedisConnectionFactory jedisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
@@ -44,20 +64,60 @@ public class RedisDome {
         return redisTemplate;
     }
 
-    public static void main(String[] args) {
-
-        RedisDome redisDome = new RedisDome();
-
-//        new
-        RedisSerializer<String> stringRedisSerializer = new StringRedisSerializer();
-        BoundValueOperations<String, String> once = redisDome.getRedisTemplate().boundValueOps("once");
-        System.out.println(
-                once.get()
-        );
-    }
 
     public RedisTemplate<String, String> getRedisTemplate() {
         return redisTemplate;
+    }
+
+
+    public RedisDome() {
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setPort(6379);
+        redisConfiguration.setHostName("192.168.79.128");
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisConfiguration);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxWaitMillis(3000);
+        jedisPoolConfig.setMaxIdle(300);
+        jedisConnectionFactory.setPoolConfig(jedisPoolConfig);
+        jedisConnectionFactory.setUsePool(true);
+
+
+        this.redisTemplate = getRedisTemplate(jedisConnectionFactory);
+        this.redisTemplate.afterPropertiesSet();
+    }
+
+}
+
+class CreateData {
+    public CreateData(RedisDome redisDome) {
+        creatAll(redisDome);
+    }
+
+
+    public CreateData(RedisDome redisDome, boolean createAll) {
+        if (createAll) {
+            creatAll(redisDome);
+        }
+    }
+
+
+    private void creatAll(RedisDome redisDome) {
+        insertStr(redisDome);
+        insertList(redisDome);
+    }
+
+    private void insertList(RedisDome redisDome) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("1");
+        strings.add("2");
+        strings.add("3");
+        redisDome.getRedisTemplate().opsForList().rightPush("Twice", strings.toString());
+
+    }
+
+    private void insertStr(RedisDome redisDome) {
+        redisDome.getRedisTemplate().opsForValue().set("once", "value");
     }
 
 }
